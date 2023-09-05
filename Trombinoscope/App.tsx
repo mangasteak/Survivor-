09b95123271e0ef684826d6f637191e0c238cb1d
Terry Mazzoni <terry.mazzoni@epitech.eu>
2023-09-05 09:43:42 +0200

    Add: Base react-native app displaying an image

---
+++ Trombinoscope/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "@react-native-material/core";
+import { DisplayAnImage } from "./Components/Image.tsx";
+
+const App = () => (
+  DisplayAnImage()
+);
+
+export default App;
---
+++ Trombinoscope/Components/Image.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Image, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+  logo: {
+    width: 300,
+    height: 300,
+    alignSelf: 'center',
+  },
+});
+
+export const DisplayAnImage = () => {
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.logo}
+        source={require('../Assets/Logo.svg')}
+      />
+    </View>
+  );
+};
